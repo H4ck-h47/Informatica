bc24c980e4340faebd7cc416fc562b2a080e4f8f
Samuel095383 <105554130+Samuel095383@users.noreply.github.com>
2025-10-03 17:30:52 +0200

    Add files via upload

---
+++ verificaTelb 01-10/es2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int nmax, nmin, n, num;
+    do{
+        cout << "Quanti numeri vuoi inserire" << endl;
+        cin >> n;
+    } while (n<0);
+    for (int i; i<n; i++){
+        cout << "Inserisci il primo numero: ";
+        cin >> num;
+        if (num > nmax){
+            nmax=num;
+        } else if (num< nmin){
+            nmin=num;
+        }
+    }
+    cout << "Il numero massimo inserito e': " << nmax << " invece quello minore e': "<< nmin;
+    return 0;
+}
